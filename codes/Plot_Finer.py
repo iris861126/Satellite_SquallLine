48dd7fafda2bdb5026a50f6dcb2f9ec000231ac6
Ariel Wu <52215353+iris861126@users.noreply.github.com>
2022-06-15 01:42:57 +0800

    Update and rename plot.py to Plot_Finer.py

--- codes/Plot_Finer.py
+++ codes/Plot_Finer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Final Project: Plotting [finer grid resolution]
+Plotting for the finer grid resolution
 """
 import numpy as np
 import matplotlib
